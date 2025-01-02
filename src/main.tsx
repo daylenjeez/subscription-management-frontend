387d9f1999fb31164c87a428986c5d970cda6ebe
daylenJeez <stussury@gmail.com>
2025-01-02 16:10:00 +0800

    Refactor routing in main.tsx to utilize dynamic routes
    - Replaced static Home component route with dynamic route mapping from the new routes module.
    - Added console log for routes to assist with debugging.
    
    These changes improve the flexibility of the routing system and prepare the application for future route management enhancements.

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { IonApp, IonRouterOutlet, IonSpinner } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
+import { routes } from "./router/routes";
 import { Route } from "react-router-dom";
-import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
+console.log(routes)
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
@@ -16,7 +17,9 @@ createRoot(document.getElementById("root")!).render(
 				<IonReactRouter>
 					<Suspense fallback={<IonSpinner name="crescent" />}>
 						<IonRouterOutlet>
-							<Route path="/" component={Home} />
+							{routes.map((route) => (
+								<Route key={route.path} path={route.path} component={route.component} exact={route.exact} />
+							))}
 						</IonRouterOutlet>
 					</Suspense>
 				</IonReactRouter>
