02067810c3eb05a0a5589b43ce298e0e85350639
daylenJeez <stussury@gmail.com>
2025-01-06 17:09:01 +0800

    Add Card component and styles for All section

---
+++ src/pages/Home/All/Card/index.tsx
@@ -0,0 +1,10 @@
+const Card = () => {
+	return <div className="all-card">
+		<div className="all-card__header">
+			<div className="all-card__logo-wrapper">
+			</div>
+		</div>
+	</div>;
+};
+
+export default Card;
